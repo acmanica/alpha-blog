4b8333740f3e0fe170520a17ed78b8a40d456345
Alvaro <acmanica@hotmail.com>
2018-12-17 14:42:40 -0600

    Show bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
 		if @article.save
-			flash[:notice] = "Article was succesfully created"
+			flash[:success] = "Article was succesfully created"
 			redirect_to article_path(@article)
 		else
 			render 'new'	
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
 	def update
 		#@article = Article.find(params[:id])
 		if @article.update(article_params)
-			flash[:notice] = "Article was succesfully update"
+			flash[:success] = "Article was succesfully update"
 			redirect_to article_path(@article)
 		else
 			render 'edit'	
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
 	def destroy
 		#@article = Article.find(params[:id])
 		@article.destroy
-		flash[:notice] =  "Article was deleted"
+		flash[:danger] =  "Article was deleted"
 		redirect_to articles_path
 	end
 
